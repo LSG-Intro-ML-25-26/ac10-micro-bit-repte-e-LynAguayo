0becde906210d246b0cee8fb3007111ad1e2e5ba
Lyn Aguayo <lyn.aguayoc@gmail.com>
2025-12-10 22:45:55 +0100

    Define repte6 function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# botó A: estacio meteorologica
+# botó B: moure la gota
+# pin0: sortir al menu
 
+mode = 0  
+
+# REPTE 6: ESTACIÓ METEOROLÒGICA
+def estacio_meteorologica():
+    global mode
+    basic.clear_screen()
+    
+    while mode == 1:
+        temp = input.temperature()
+        
+        # mostrar grafic (max 50 graus)
+        led.plot_bar_graph(temp, 50)
+        
+        basic.pause(1000)
--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
+//  botó A: estacio meteorologica
+//  botó B: moure la gota
+//  pin0: sortir al menu
+let mode = 0
+//  REPTE 6: ESTACIÓ METEOROLÒGICA
+function estacio_meteorologica() {
+    let temp: number;
+    
+    basic.clearScreen()
+    while (mode == 1) {
+        temp = input.temperature()
+        //  mostrar grafic (max 50 graus)
+        led.plotBarGraph(temp, 50)
+        basic.pause(1000)
+    }
+}
 
